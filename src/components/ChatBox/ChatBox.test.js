f6c6fc23eccb68e4867d58c38546f06645f8b37d
Renemari Padillo <padillo.renemari@gmail.com>
2017-04-17 22:27:10 +0800

    [CHORE] Added unit-test for ChatBox Component

--- src/components/ChatBox/ChatBox.test.js
+++ src/components/ChatBox/ChatBox.test.js
@@ -1,8 +1,42 @@
 import React from 'react';
+import { shallow } from 'enzyme';
 import ReactDOM from 'react-dom';
 import ChatBox from './ChatBox';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<ChatBox />, div);
+describe('<Chatbox />', () => {
+    let chatbox;
+
+    beforeEach(() => {
+        const props = {
+            user: 'test',
+            messages: [
+                {
+                    id: 1,
+                    author: 'Tester1',
+                    content: 'Hello World!'
+                },
+                {
+                    id: 2,
+                    author: 'Tester2',
+                    content: 'Hi!'
+                },
+                {
+                    id: 3,
+                    author: 'Tester1',
+                    content: 'Hello!'
+                }
+            ],
+            sendMessage: () => {}
+        };
+
+        chatbox = shallow(<ChatBox {...props} />);
+    });
+
+    // DOM
+    it('should render <ChatBoxControls /> component', () => {
+        expect(chatbox.find('ChatBoxControls')).toHaveLength(1);
+    });
+    it('should render <ChatBoxCard /> components', () => {
+        expect(chatbox.find('ChatBoxCard').length).toBeGreaterThan(0);
+    });
 });
--- src/services/WebsocketService.test.js
+++ src/services/WebsocketService.test.js
@@ -1,1 +1,11 @@
 import WebsocketService from './WebsocketService';
+
+describe('WebsocketService', () => {
+    it('should not be undefined', () => {
+        expect(WebsocketService).not.toBe(undefined);
+    });
+    it('should be able instantiate', () => {
+        const websocket = new WebsocketService('ws://127.0.0.1:1337');
+        expect(websocket).not.toBe(undefined);
+    });
+});
