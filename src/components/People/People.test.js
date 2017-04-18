79ad8693b241f51a201e1c35a74e164a9c081b43
Renemari Padillo <padillo.renemari@gmail.com>
2017-04-18 17:32:17 +0800

    [CHORE] Added DOM unit-test

--- src/components/People/People.test.js
+++ src/components/People/People.test.js
@@ -1,8 +1,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import People from './People';
+import { shallow } from 'enzyme';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<People />, div);
+describe('<People />', () => {
+    let props;
+
+    beforeEach(() => {
+        props = {
+            people: []
+        };
+    });
+
+    // DOM
+    it('renders without crashing', () => {
+        const div = document.createElement('div');
+        ReactDOM.render(<People {...props} />, div);
+    });
+    it('should display the list of people', () => {
+        props.people = [
+            { id: 1, username: 'User1' },
+            { id: 2, username: 'User2' },
+            { id: 3, username: 'User3' }
+        ];
+
+        const component = shallow(
+            <People {...props} />
+        );
+
+        expect(component.find('.online li')).toHaveLength(props.people.length);
+    });
 });
