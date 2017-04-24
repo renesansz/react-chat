362f5fd52fc78f65cb48eb7c41307948d11ba2a0
Renemari Padillo <padillo.renemari@gmail.com>
2017-04-24 18:40:17 +0800

    🚧 Refactor unit-test scripts

--- src/App.test.js
+++ src/App.test.js
@@ -1,10 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+import { mount } from 'enzyme';
+
 import App from './App';
 
 describe('<App />', () => {
-  it('renders without crashing', () => {
-    const div = document.createElement('div');
-    ReactDOM.render(<App />, div);
+
+  describe('constructor()', () => {
+    it('should have an initial empty array value for `messages` state', () => {
+      const component = mount(<App />);
+      expect(component.state().messages).toBe([]);
+    });
+    it('should have an initial empty array value for `people` state', () => {
+      const component = mount(<App />);
+      expect(component.state().people).toBe([]);
+    });
+    it('should have an initial null value for `user` state', () => {
+      const component = mount(<App />);
+      expect(component.state().messages).toBe(null);
+    });
+    it('should have an initial false boolean value for `isLoggedIn` state', () => {
+      const component = mount(<App />);
+      expect(component.state().isLoggedIn).toBe(false);
+    });
   });
+
 });
--- src/services/WebsocketService.test.js
+++ src/services/WebsocketService.test.js
@@ -1,11 +1,35 @@
+import { WebSocket } from 'mock-socket';
+
 import WebsocketService from './WebsocketService';
 
 describe('WebsocketService', () => {
+
+    beforeEach(() => {
+        window.WebSocket = WebSocket; // Mock the window.WebSocket
+    });
+
+    describe('constructor()', () => {
+        it('requires 1 string argument', () => {
+            expect(() => {
+                new WebsocketService()
+            }).toThrow();
+            expect(() => {
+                new WebsocketService(1)
+            }).toThrow();
+            expect(() => {
+                new WebsocketService(1.0)
+            }).toThrow();
+            expect(() => {
+                new WebsocketService(true)
+            }).toThrow();
+        });
+    });
+
     it('should not be undefined', () => {
         expect(WebsocketService).not.toBe(undefined);
     });
-    it('should be able instantiate', () => {
-        const websocket = new WebsocketService('ws://127.0.0.1:1337');
-        expect(websocket).not.toBe(undefined);
-    });
+    it('should be able to connect', () => {});
+    it('should be able to send message', () => {});
+    it('should be able to receive message', () => {});
+
 });
